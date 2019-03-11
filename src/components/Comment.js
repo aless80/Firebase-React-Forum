8aaa788dfb983520ebf0c3ab707b54bad712f681
Alessandro Marin <AlessandroMarin80@gmail.com>
2019-03-11 18:18:07 +0100

    Comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,20 +2,6 @@ import React, { Component } from "react";
 import "./Comment.css";
 
 class Comment extends Component {
-  state = {
-    comment: {},
-    comment_key: undefined,
-    post_title: ""
-  };
-
-  componentDidMount() {
-    console.log(this.props);
-    this.setState({
-      comment: this.props.comment,
-      comment_key: this.props.comment.key,
-      post_title: this.props.post_title
-    });
-  }
 
   printDate(timestamp) {
     if (!timestamp) return "";
@@ -36,6 +22,11 @@ class Comment extends Component {
       pad(dateObj.getMinutes())
     );
   }
+
+  remainderTo2(id) {
+    if (id%2) return 1
+    return 2
+  }
   render() {
     const hello = {
       flex: "1 1 100px"
@@ -45,9 +36,9 @@ class Comment extends Component {
         <div align="center">
           <div className="page text-left w-100">
             <div className="p-0" align="left">
-              <div id="edit5971042" className="p-0">
+              <div id={"edit"+this.props.comment.id} className="p-0">
                 <table
-                  id="post5971042"
+                  id={"post"+this.props.comment.id}
                   className="tborder w-100 align-middle"
                   cellSpacing="0"
                   cellPadding="3"
@@ -66,10 +57,10 @@ class Comment extends Component {
                           <span className="float-right">
                             &nbsp; #
                             <a
-                              href="/questions/linux-newbie-8/chrome-browser-on-linux-mint-4175649625/#post5971042"
+                              href={"#reply"+this.props.comment.id}
                               rel="nofollow"
-                              id="postcount5971042"
-                              name="7"
+                              id={"reply"+this.props.comment.id}
+                              name={this.props.comment.id}
                             >
                               <strong>{this.props.comment.id}</strong>
                             </a>
@@ -80,36 +71,40 @@ class Comment extends Component {
                   </thead>
                   <tbody>
                     <tr className="align-top">
-                      <td className="alt2 border" width="175">
-                        <div id="postmenu_5971042">
+                      <td className={"alt" + this.remainderTo2(this.props.comment.id) + " border"} width="175">
+                        <div id={"postmenu_"+this.props.comment.id}>
                           <div className="font-weight-strong">
                             {this.props.comment.author}
                           </div>
                         </div>
                         <div className="small text-muted">
                           &nbsp;
+                          {/*
                           <br />
                           <div>Registered: </div>
+                          <div>Latest activity: </div>
+                          Need Admin SDK in priviledge environment
+                          https://firebase.google.com/docs/admin/setup                          
+                          */}
                         </div>
                       </td>
-                      <td className="alt2 border" id="td_post_5971042">
-                        <div id="post_message_5971042">
+                      <td className={"alt" + this.remainderTo2(this.props.comment.id) + " border"} id={"td_post_"+this.props.comment.id}>
+                        <div id={"post_message_"+this.props.comment.id}>
                           {this.props.comment.text}
                         </div>
 
                         <div className="small text-muted" style={hello}>
                           <div className="post-menu">
                             <a
-                              href="/posts/54687061/edit"
+                              href={"/posts/"+this.props.comment.id+"/edit"}
                               className="suggest-edit-post"
                             >
                               edit
                             </a>
                             <span>&nbsp;</span>
-                            <a
+                            {/*<a
                               href="#"
                               className="close-question-link"
-                              data-questionid="54675211"
                               data-isclosed="false"
                             >
                               delete
@@ -118,10 +113,9 @@ class Comment extends Component {
                             <a
                               href="#"
                               className="flag-post-link"
-                              data-postid="54687061"
                             >
                               flag
-                            </a>
+                            </a>*/}
                           </div>
                         </div>
                       </td>
